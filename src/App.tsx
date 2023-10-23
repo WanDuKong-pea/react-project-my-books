13deaf8b446764e4b57634088f61eacdd46b0ea0
KimYeJin <sophisticateo_o@naver.com>
2023-10-23 19:52:47 +0900

    FIRST_COMMIT: 프로젝트 설정 및 Router설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Add from './pages/Add';
+import Signin from './pages/Signin';
+import Edit from './pages/Edit';
+import Detail from './pages/Detail';
+import NotFound from './pages/NotFound';
+import { ErrorBoundary } from 'react-error-boundary';
+import Error from './pages/Error';
 
+//<Route>의 exact 속성은 V6에서는 제거되었으며, 기본값이 true로 변경되었음
+//<Switch>또한 V6에서 <Routes>로 변경되었음
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/edit:id" element={<Edit />} />
+          <Route path="/book:id" element={<Detail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
